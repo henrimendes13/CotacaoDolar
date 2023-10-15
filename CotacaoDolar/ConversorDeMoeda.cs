421260746260f2548410535e83eb353b8bfb6540
Henrique Mendes <henriquemendes13@gmail.com>
2023-10-15 20:47:57 -0300

    Add project files.

---
+++ CotacaoDolar/ConversorDeMoeda.cs
@@ -0,0 +1,11 @@
+﻿namespace CotacaoDolar
+{
+    class ConversorDeMoeda
+    {
+        public static double Cotacao, QuatidadeDolares;
+        public static double ValorASerPago()
+        {
+            return QuatidadeDolares*Cotacao*1.06;
+        }
+    }
+}
---
+++ CotacaoDolar/Program.cs
@@ -0,0 +1,10 @@
+﻿using CotacaoDolar;
+
+Console.Write("QUAL É A COTAÇÃO DO DOLAR? ");
+ConversorDeMoeda.Cotacao = double.Parse(Console.ReadLine());
+
+Console.Write("QUANTOS DOLARES VOCÊ VAI COMPRAR? ");
+ConversorDeMoeda.QuatidadeDolares = double.Parse(Console.ReadLine());
+
+Console.Write("VALOR A SER PAGO EM REAIS: ");
+Console.WriteLine(ConversorDeMoeda.ValorASerPago());
